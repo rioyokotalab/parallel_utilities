d6ddadaa97873694579d3bacf7d38a26d934cbac
Hiroki11x <hiroki11x@gmail.com>
2018-04-04 14:23:06 +0900

    fix test

--- tests/nccl_allreduce.py
+++ tests/nccl_allreduce.py
@@ -5,6 +5,7 @@ import unittest
 
 import chainermn
 import mpi4py.MPI
+import numpy as np
 
 class ExampleModel(chainer.Chain):
     def __init__(self, n_in=3, n_units=5, n_out=2):
@@ -19,25 +20,23 @@ class ExampleModel(chainer.Chain):
 
 class TestAllreducePersistent(unittest.TestCase):
 
-    def _test(self, comm, model):
+    def _test(self, comm, color_value, model):
+        print("color_value : {} / comm.intra_rank : {}".format(color_value,comm.intra_rank))
         rank = comm.rank
-        #model.bn1.avg_mean.fill(rank * 1)
-        #model.bn2.avg_mean.fill(rank * 2)
-        #model.bn1.avg_var.fill(rank * 3)
-        #model.bn2.avg_var.fill(rank * 4)
+        model.bn1.avg_mean.fill(rank * 1)
+        model.bn2.avg_mean.fill(rank * 2)
+        model.bn1.avg_var.fill(rank * 3)
+        model.bn2.avg_var.fill(rank * 4)
 
-        #allreduce_persistent = \
-        #    chainermn.extensions.AllreducePersistent(model, comm)
-        #allreduce_persistent()
+        allreduce_persistent = \
+            chainermn.extensions.AllreducePersistent(model, comm)
+        allreduce_persistent()
 
-        #avg_rank = (comm.size - 1) / 2.0
-        #chainer.testing.assert_allclose(model.bn1.avg_mean, avg_rank * 1)
-        #chainer.testing.assert_allclose(model.bn2.avg_mean, avg_rank * 2)
-        #chainer.testing.assert_allclose(model.bn1.avg_var, avg_rank * 3)
-        #chainer.testing.assert_allclose(model.bn2.avg_var, avg_rank * 4)
-        #print("model.bn1.avg_mean:{}".format(model.bn1.avg_mean))
-        #print("avg_rank:{}".format(avg_rank))
-        print("rank:{}".format(comm.rank))
+        avg_rank = (comm.size - 1) / 2.0
+        chainer.testing.assert_allclose(model.bn1.avg_mean, avg_rank * 1)
+        chainer.testing.assert_allclose(model.bn2.avg_mean, avg_rank * 2)
+        chainer.testing.assert_allclose(model.bn1.avg_var, avg_rank * 3)
+        chainer.testing.assert_allclose(model.bn2.avg_var, avg_rank * 4)
 
     def test_allreduce_persistent_gpu(self):
         mpi_comm = mpi4py.MPI.COMM_WORLD
@@ -47,18 +46,18 @@ class TestAllreducePersistent(unittest.TestCase):
 
         color_value = wcomm.rank//2
         key_value = wcomm.rank%2
-        print("wcomm.rank : {} / color_value : {} / key_value :  {}".format(wcomm.rank,color_value,key_value))
+        # print("wcomm.rank : {} / color_value : {} / key_value :  {}".format(wcomm.rank,color_value,key_value))
 
-        #scomm = wcomm.split(color=color_value, key=key_value)
         device = wcomm.intra_rank
-        #device = wcomm.intra_rank*2+scomm.intra_rank
         chainer.cuda.get_device(device).use()
-        #print("wcomm.rank : {} / scomm.rank : {}".format(wcomm.rank,scomm.rank))
-        # print("wcomm.intra_rank : {} / scomm.intra_rank : {}".format(wcomm.intra_rank,scomm.intra_rank))
-        # print("wcomm.mpi_comm.rank : {} / scomm.mpi_comm.rank : {}".format(wcomm.mpi_comm.rank,scomm.mpi_comm.rank))
+
+        scomm = wcomm.split(color=color_value, key=key_value)
+        # print("wcomm.rank : {} / scomm.rank : {}".format(wcomm.rank, scomm.rank))
+        
         model = ExampleModel()
         model.to_gpu()
-        self._test(wcomm, model)
+        self._test(scomm, color_value, model)
+        # self._test(wcomm, color_value, model)
 
 if __name__ == '__main__':
     unittest.main()
